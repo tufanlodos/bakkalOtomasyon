839df47ef089c91b8ec9005ae58669c78df253d6
720694-Ogle <720694-Ogle@TURGAY-PC>
2019-01-21 20:48:02 +0300

    anasayfa formu temizlendi

--- bakkalOtomasyon/Form1.Designer.cs
+++ bakkalOtomasyon/Form1.Designer.cs
@@ -35,8 +35,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
