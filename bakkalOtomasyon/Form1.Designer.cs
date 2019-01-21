86d30ef56b52e37e594bc4648a742268a000d5e5
720694-Ogle <720694-Ogle@TURGAY-PC>
2019-01-21 20:07:39 +0300

    dgvler ve buton atıldı

--- bakkalOtomasyon/Form1.Designer.cs
+++ bakkalOtomasyon/Form1.Designer.cs
@@ -28,48 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvKategoriler = new System.Windows.Forms.DataGridView();
-            this.btnGetir = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvKategoriler)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvKategoriler
-            // 
-            this.dgvKategoriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvKategoriler.Location = new System.Drawing.Point(135, 163);
-            this.dgvKategoriler.Name = "dgvKategoriler";
-            this.dgvKategoriler.Size = new System.Drawing.Size(531, 171);
-            this.dgvKategoriler.TabIndex = 3;
-            // 
-            // btnGetir
-            // 
-            this.btnGetir.Location = new System.Drawing.Point(236, 116);
-            this.btnGetir.Name = "btnGetir";
-            this.btnGetir.Size = new System.Drawing.Size(117, 23);
-            this.btnGetir.TabIndex = 2;
-            this.btnGetir.Text = "Kategorileri Getir";
-            this.btnGetir.UseVisualStyleBackColor = true;
-            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvKategoriler);
-            this.Controls.Add(this.btnGetir);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvKategoriler)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgvKategoriler;
-        private System.Windows.Forms.Button btnGetir;
     }
 }
 
