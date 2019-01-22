90df96d5fe752531e3d163f4e1a538e3f8572bbf
720694-Ogle <720694-Ogle@TURGAY-PC>
2019-01-22 18:22:48 +0300

    hatalı assemblyler düzeltildi, migration kontrolü eklendi

---
+++ DAL.Bakkal/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace DAL.Bakkal.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.Bakkal.DataModel.BCDContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "DAL.Bakkal.DataModel.BCDContext";
+        }
+
+        protected override void Seed(DAL.Bakkal.DataModel.BCDContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
