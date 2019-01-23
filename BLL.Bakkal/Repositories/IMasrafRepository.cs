ff6f612eebc1029c05663f6f3736a17d1c21e0a8
mertcelil <M720694@outlook.com>
2019-01-24 00:54:55 +0300

    interfaceler yazılıp implement edildi

--- BLL.Bakkal/Repositories/IMasrafRepository.cs
+++ BLL.Bakkal/Repositories/IMasrafRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Bakkal.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace BLL.Bakkal.Repositories
 {
     public interface IMasrafRepository
     {
+        List<Masraf> MasraflariGetir();
+        bool MasrafEkle(Masraf masraf);
+        bool MasrafDegistir(int Id);
+        bool MasrafSil(int Id);
     }
 }
--- BLL.Bakkal/Repositories/MasrafRepository.cs
+++ BLL.Bakkal/Repositories/MasrafRepository.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Bakkal.DataModel;
 
 namespace BLL.Bakkal.Repositories
 {
-    class MasrafRepository
+    public class MasrafRepository : IMasrafRepository
     {
+        public List<Masraf> MasraflariGetir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MasrafEkle(Masraf masraf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MasrafDegistir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MasrafSil(int Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
