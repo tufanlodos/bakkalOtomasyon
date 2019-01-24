8892a821e9d70b47206bd0835fe4901184772c8c
mertcelil <M720694@outlook.com>
2019-01-24 03:55:46 +0300

    starta basınca masraf formunun çalışması sağlandı

--- bakkalOtomasyon/Program.cs
+++ bakkalOtomasyon/Program.cs
@@ -22,7 +22,7 @@ namespace PL.Bakkal
             }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Anasayfa());
+            Application.Run(new Masraf());
         }
     }
 }
