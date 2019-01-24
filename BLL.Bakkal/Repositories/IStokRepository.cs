a8a7977c32abb8b2bc106d929927266c994ab652
demet <720694-Ogle@WISSEN-PC>
2019-01-24 17:32:48 +0300

    stok repository için interface ve class oluşturuldu.

---
+++ BLL.Bakkal/Repositories/IStokRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Bakkal.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Bakkal.Repositories
+{
+    public interface IStokRepository
+    {
+        List<Kategoriler> KategoriGetir();
+        
+        
+        List<Urunler> UrunGetirByKategoriID(int ID);
+       
+    }
+}
