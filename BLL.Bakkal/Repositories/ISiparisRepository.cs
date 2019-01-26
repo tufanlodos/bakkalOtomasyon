9fe6b00b289ca5c5e3fc298712d9057db98b90ff
mertcelil <M720694@outlook.com>
2019-01-26 17:38:54 +0300

    interface ve class repository leri yazıldı.

--- BLL.Bakkal/Repositories/ISiparisRepository.cs
+++ BLL.Bakkal/Repositories/ISiparisRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Bakkal.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace BLL.Bakkal.Repositories
 {
     public interface ISiparisRepository
     {
+        List<Urunler> UrunleriGetir();
+        List<Urunler> UrunleriGetirByKategoriId(int KategoriId);
+        List<Kategoriler> KategorileriGetir();
+        List<Kategoriler> KategorileriGetirByUrunId(int UrunId);
+        List<Tedarikci> TedarikcileriGetir();
+        int UrunFiyatiGetirById(int UrunId);
+        int ToplamTutarHesapla();
+        bool SiparisEkle();
+        bool SiparisGuncelle();
+        bool SiparisSil();
     }
 }
--- BLL.Bakkal/Repositories/SiparisRepository.cs
+++ BLL.Bakkal/Repositories/SiparisRepository.cs
@@ -3,11 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Bakkal.DataModel;
 
 namespace BLL.Bakkal.Repositories
 {
     public class SiparisRepository : ISiparisRepository
     {
+        BCDContext ent = new BCDContext();
 
+        public List<Kategoriler> KategorileriGetir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Kategoriler> KategorileriGetirByUrunId(int UrunId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SiparisEkle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SiparisGuncelle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SiparisSil()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tedarikci> TedarikcileriGetir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ToplamTutarHesapla()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UrunFiyatiGetirById(int UrunId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Urunler> UrunleriGetir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Urunler> UrunleriGetirByKategoriId(int KategoriId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
