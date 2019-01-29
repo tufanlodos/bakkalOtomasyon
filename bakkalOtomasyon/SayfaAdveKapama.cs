43c24aa93ef508bb3db80e7e0928670a5aa3856e
mrc <720694-Ogle@TURGAY-PC>
2019-01-29 17:12:02 +0300

    sayfa adı ve sayfa kapatması için user control yazıldı.

---
+++ bakkalOtomasyon/SayfaAdveKapama.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL.Bakkal
+{
+    public partial class SayfaAdveKapama : UserControl
+    {
+        public SayfaAdveKapama()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.ParentForm.Close();
+        }
+    }
+}
