b3265556375c765f9cf46ac3efe86c472be93ae4
mertcelil <M720694@outlook.com>
2019-01-30 02:24:57 +0300

    frmkategoriler ve frmkategoriekle formları tamamlandı

--- bakkalOtomasyon/frmKategoriler.Designer.cs
+++ bakkalOtomasyon/frmKategoriler.Designer.cs
@@ -35,6 +35,8 @@
             this.lblKategoriAdiGiriniz = new System.Windows.Forms.Label();
             this.btnVazgec = new System.Windows.Forms.Button();
             this.btnGuncelle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.formKapatici1 = new PL.Bakkal.FormKapatici();
             ((System.ComponentModel.ISupportInitialize)(this.dgvKategori)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,17 +46,19 @@
             this.dgvKategori.AllowUserToDeleteRows = false;
             this.dgvKategori.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvKategori.GridColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.dgvKategori.Location = new System.Drawing.Point(21, 43);
+            this.dgvKategori.Location = new System.Drawing.Point(21, 81);
             this.dgvKategori.Name = "dgvKategori";
             this.dgvKategori.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvKategori.Size = new System.Drawing.Size(440, 156);
             this.dgvKategori.TabIndex = 33;
+            this.dgvKategori.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKategori_CellDoubleClick);
+            this.dgvKategori.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKategori_CellValueChanged);
             this.dgvKategori.DoubleClick += new System.EventHandler(this.dgvKategori_DoubleClick);
             // 
             // btnSil
             // 
             this.btnSil.Enabled = false;
-            this.btnSil.Location = new System.Drawing.Point(230, 216);
+            this.btnSil.Location = new System.Drawing.Point(230, 254);
             this.btnSil.Name = "btnSil";
             this.btnSil.Size = new System.Drawing.Size(81, 29);
             this.btnSil.TabIndex = 32;
@@ -64,7 +68,7 @@
             // 
             // btnYeni
             // 
-            this.btnYeni.Location = new System.Drawing.Point(21, 216);
+            this.btnYeni.Location = new System.Drawing.Point(21, 254);
             this.btnYeni.Name = "btnYeni";
             this.btnYeni.Size = new System.Drawing.Size(81, 29);
             this.btnYeni.TabIndex = 1;
@@ -74,16 +78,16 @@
             // 
             // txtArama
             // 
-            this.txtArama.Location = new System.Drawing.Point(126, 12);
+            this.txtArama.Location = new System.Drawing.Point(126, 50);
             this.txtArama.Name = "txtArama";
             this.txtArama.Size = new System.Drawing.Size(112, 20);
-            this.txtArama.TabIndex = 0;
+            this.txtArama.TabIndex = 1;
             this.txtArama.TextChanged += new System.EventHandler(this.txtArama_TextChanged);
             // 
             // lblKategoriAdiGiriniz
             // 
             this.lblKategoriAdiGiriniz.AutoSize = true;
-            this.lblKategoriAdiGiriniz.Location = new System.Drawing.Point(18, 15);
+            this.lblKategoriAdiGiriniz.Location = new System.Drawing.Point(18, 53);
             this.lblKategoriAdiGiriniz.Name = "lblKategoriAdiGiriniz";
             this.lblKategoriAdiGiriniz.Size = new System.Drawing.Size(95, 13);
             this.lblKategoriAdiGiriniz.TabIndex = 55;
@@ -91,7 +95,7 @@
             // 
             // btnVazgec
             // 
-            this.btnVazgec.Location = new System.Drawing.Point(317, 216);
+            this.btnVazgec.Location = new System.Drawing.Point(317, 254);
             this.btnVazgec.Name = "btnVazgec";
             this.btnVazgec.Size = new System.Drawing.Size(86, 29);
             this.btnVazgec.TabIndex = 57;
@@ -102,7 +106,7 @@
             // 
             // btnGuncelle
             // 
-            this.btnGuncelle.Location = new System.Drawing.Point(108, 217);
+            this.btnGuncelle.Location = new System.Drawing.Point(108, 255);
             this.btnGuncelle.Name = "btnGuncelle";
             this.btnGuncelle.Size = new System.Drawing.Size(116, 28);
             this.btnGuncelle.TabIndex = 58;
@@ -110,11 +114,32 @@
             this.btnGuncelle.UseVisualStyleBackColor = true;
             this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
-            // Kategori
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 13);
+            this.label1.TabIndex = 123;
+            this.label1.Text = "KATEGORİLER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // formKapatici1
+            // 
+            this.formKapatici1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.formKapatici1.Location = new System.Drawing.Point(0, 0);
+            this.formKapatici1.Name = "formKapatici1";
+            this.formKapatici1.Size = new System.Drawing.Size(473, 29);
+            this.formKapatici1.TabIndex = 59;
+            // 
+            // frmKategoriler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(473, 296);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formKapatici1);
             this.Controls.Add(this.btnGuncelle);
             this.Controls.Add(this.btnVazgec);
             this.Controls.Add(this.txtArama);
@@ -122,7 +147,8 @@
             this.Controls.Add(this.dgvKategori);
             this.Controls.Add(this.btnSil);
             this.Controls.Add(this.btnYeni);
-            this.Name = "Kategori";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmKategoriler";
             this.Text = "Kategori";
             this.Load += new System.EventHandler(this.Kategori_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvKategori)).EndInit();
@@ -139,5 +165,7 @@
         private System.Windows.Forms.Label lblKategoriAdiGiriniz;
         private System.Windows.Forms.Button btnVazgec;
         private System.Windows.Forms.Button btnGuncelle;
+        private FormKapatici formKapatici1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- bakkalOtomasyon/frmKategoriler.cs
+++ bakkalOtomasyon/frmKategoriler.cs
@@ -21,7 +21,7 @@ namespace PL.Bakkal
 
         KategoriRepository Kr = new KategoriRepository();
         BCDContext ent = new BCDContext();
-        int ID;
+        int ID,HeaderText=0;
 
         private void Kategori_Load(object sender, EventArgs e)
         {
@@ -32,20 +32,27 @@ namespace PL.Bakkal
         private void DgvDoldurDuzenle()
         {
             dgvKategori.DataSource = Kr.KategoriListele();
-            dgvKategori.Columns[0].Visible = false;
-            dgvKategori.Columns[3].Visible = false;
             dgvKategori.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             dgvKategori.AllowUserToAddRows = false;
             dgvKategori.AllowUserToDeleteRows = false;
             dgvKategori.AllowUserToResizeColumns = false;
             dgvKategori.AllowUserToResizeRows = false;
             dgvKategori.RowHeadersVisible = false;
+            dgvKategori.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            dgvKategori.MultiSelect = false;
             dgvKategori.BorderStyle = BorderStyle.None;
             dgvKategori.BackgroundColor = this.BackColor;
+            dgvKategori.Columns[0].Visible = false;
+            dgvKategori.Columns[1].HeaderText = "Kategori Adı";
+            dgvKategori.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKategori.Columns[2].HeaderText = "Açıklama";
+            dgvKategori.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKategori.Columns[3].Visible = false;
+            dgvKategori.ClearSelection();
+            txtArama.Text = "";
             btnGuncelle.Enabled = false;
             btnSil.Enabled = false;
-            dgvKategori.ClearSelection();
-
+            HeaderText++;
         }
         private void btnSil_Click(object sender, EventArgs e)
         {
@@ -55,58 +62,81 @@ namespace PL.Bakkal
                 {
                     MessageBox.Show("Kategori bilgileri silindi.", "Silme gerçekleşti.");
                     dgvKategori.DataSource = Kr.KategoriListele();
-
                     btnSil.Enabled = false;
                     btnYeni.Enabled = true;
+                    btnVazgec.Visible = false;
+                    btnGuncelle.Enabled = false;
                 }
             }
         }
-
+        int SecilenSatir;
+        private void dgvKategori_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SecilenSatir = e.RowIndex;
+        }
         private void dgvKategori_DoubleClick(object sender, EventArgs e)
         {
-            ID = Convert.ToInt32(dgvKategori.SelectedRows[0].Cells[0].Value);
-            btnSil.Enabled = true;
-            btnYeni.Enabled = false;
-            btnGuncelle.Enabled = true;
-            btnVazgec.Visible = true;
+            try
+            {
+                ID = Convert.ToInt32(dgvKategori.SelectedRows[0].Cells[0].Value);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+            if (ID > 0)
+            {
+                this.AcceptButton = btnSil;
+                btnSil.Enabled = true;
+                btnYeni.Enabled = false;
+                btnGuncelle.Enabled = false;
+                btnVazgec.Visible = true;
+            }
+            if (SecilenSatir >= 0)
+            {
+                dgvKategori.Rows[SecilenSatir].DefaultCellStyle.BackColor = Color.White;
+            }
+            if (!btnGuncelle.Enabled)
+            {
+                dgvKategori.SelectedRows[0].DefaultCellStyle.BackColor = Color.Red;
+            }
         }
 
+        private void dgvKategori_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (HeaderText >= 1)
+            {
+                dgvKategori.SelectedRows[0].DefaultCellStyle.BackColor = Color.White;
+                dgvKategori.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.ForeColor = Color.Red;
+                btnGuncelle.Enabled = true;
+                this.AcceptButton = btnGuncelle;
+                btnSil.Enabled = false;
+                btnVazgec.Visible = true;
+            }
+        }
         private void btnEkle_Click(object sender, EventArgs e)
         {
-            //if (!string.IsNullOrEmpty(txtKategoriAdi.Text))
-            //{
-            //    Kategoriler yeni = new Kategoriler();
-            //    yeni.KategoriAdi = txtKategoriAdi.Text;
-            //    if (Kr.KategoriKontrol(yeni))
-            //    {
-            //        MessageBox.Show("Bu kategori kayıtlı!", "Aynı kategori zaten var!");
-            //    }
-            //    else
-            //    {
-            //        yeni.Aciklama = txtAciklama.Text;
-            //        if (Kr.KategoriEkle(yeni))
-            //        {
-            //            MessageBox.Show("Yeni kategori eklendi.", "Kayıt gerçekleşti.");
-            //            dgvKategori.DataSource = Kr.KategoriListele();
-
-            //            Temizle();
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Kategori ismi girilmelidir!", "Dikkat! Eksik Bilgi!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //}
-            //Temizle();
-            //txtKategoriAdi.Focus();
+            btnGuncelle.Enabled = false;
+            btnSil.Enabled = false;
+            btnVazgec.Visible = false;
+            frmKategoriEkle frm = new frmKategoriEkle();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.ShowDialog();
+            DgvDoldurDuzenle();
+            txtArama.Focus();
+            this.AcceptButton = btnYeni;
         }
 
         private void btnVazgec_Click(object sender, EventArgs e)
         {
+            Kr.ContextteBekleyenleriTemizle();
             btnSil.Enabled = false;
             btnYeni.Enabled = true;
             btnGuncelle.Enabled = false;
             btnVazgec.Visible = false;
+            DgvDoldurDuzenle();
+            txtArama.Focus();
+            this.AcceptButton = btnYeni;
         }
 
         private void btnGuncelle_Click(object sender, EventArgs e)
@@ -116,20 +146,23 @@ namespace PL.Bakkal
                 MessageBox.Show("Kategori kayıtları üzerindeki güncellemeler kaydedildi.", "İşlem tamamlandı", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 DgvDoldurDuzenle();
                 btnGuncelle.Enabled = false;
+                btnVazgec.Visible = false;
+                btnYeni.Enabled = true;
+                this.AcceptButton = btnYeni;
+                txtArama.Focus();
             }
             else
             {
                 MessageBox.Show("Kategori kayıtları üzerindeki güncellemeler kaydedilemedi", "İşlem tamamlanamadı", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 DgvDoldurDuzenle();
-
             }
-
-
         }
+
         private void txtArama_TextChanged(object sender, EventArgs e)
         {
             dgvKategori.DataSource = Kr.KategoriGetirByArama(txtArama.Text);
         }
+        
     }
 }
 
