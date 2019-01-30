ac7b749d9fdd86e9ca736dc8c850d1aedc0bf1d1
cihan <cihan.oguz@windowslive.com>
2019-01-30 16:56:28 +0300

    vazgeçildi

---
+++ bakkalOtomasyon/frmUrunEkle.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PL.Bakkal
+{
+    partial class frmUrunEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKategoriAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrunMarkasi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAlisFiyati = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSatisFiyati = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori Adı";
+            // 
+            // txtKategoriAdi
+            // 
+            this.txtKategoriAdi.Location = new System.Drawing.Point(106, 30);
+            this.txtKategoriAdi.Name = "txtKategoriAdi";
+            this.txtKategoriAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKategoriAdi.TabIndex = 1;
+            this.txtKategoriAdi.TextChanged += new System.EventHandler(this.txtKategoriAdi_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ürün Markası";
+            // 
+            // txtUrunMarkasi
+            // 
+            this.txtUrunMarkasi.Location = new System.Drawing.Point(106, 65);
+            this.txtUrunMarkasi.Name = "txtUrunMarkasi";
+            this.txtUrunMarkasi.Size = new System.Drawing.Size(100, 20);
+            this.txtUrunMarkasi.TabIndex = 1;
+            this.txtUrunMarkasi.TextChanged += new System.EventHandler(this.txtUrunMarkasi_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ürün Adi";
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(106, 100);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtUrunAdi.TabIndex = 1;
+            this.txtUrunAdi.TextChanged += new System.EventHandler(this.txtUrunAdi_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Birim Alış Fiyatı";
+            // 
+            // txtAlisFiyati
+            // 
+            this.txtAlisFiyati.Location = new System.Drawing.Point(106, 135);
+            this.txtAlisFiyati.Name = "txtAlisFiyati";
+            this.txtAlisFiyati.Size = new System.Drawing.Size(100, 20);
+            this.txtAlisFiyati.TabIndex = 1;
+            this.txtAlisFiyati.TextChanged += new System.EventHandler(this.txtAlisFiyati_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Birim Satış Fiyatı";
+            // 
+            // txtSatisFiyati
+            // 
+            this.txtSatisFiyati.Location = new System.Drawing.Point(106, 170);
+            this.txtSatisFiyati.Name = "txtSatisFiyati";
+            this.txtSatisFiyati.Size = new System.Drawing.Size(100, 20);
+            this.txtSatisFiyati.TabIndex = 1;
+            this.txtSatisFiyati.TextChanged += new System.EventHandler(this.txtSatisFiyati_TextChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(70, 225);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmUrunEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 286);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSatisFiyati);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAlisFiyati);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUrunMarkasi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKategoriAdi);
+            this.Controls.Add(this.label1);
+            this.Name = "frmUrunEkle";
+            this.Text = "frmUrunEkle";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKategoriAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrunMarkasi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAlisFiyati;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSatisFiyati;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ bakkalOtomasyon/frmUrunEkle.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL.Bakkal
+{
+    public partial class frmUrunEkle : Form
+    {
+        public frmUrunEkle()
+        {
+            InitializeComponent();
+        }
+        //UrunRepository ur = new UrunRepository();
+        private void txtKategoriAdi_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            foreach (char harf in txtKategoriAdi.Text)
+            {
+                if (!char.IsLetter(harf))
+                {
+                    if (harf == ' ')
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu alana sadece harf girişi yapılabilir.");
+                        txtKategoriAdi.Text = txtKategoriAdi.Text.Substring(0, txtKategoriAdi.Text.Length - 1);
+                        txtKategoriAdi.Select(txtKategoriAdi.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void txtUrunMarkasi_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            foreach (char harf in txtUrunMarkasi.Text)
+            {
+                if (!char.IsLetter(harf))
+                {
+                    if (harf == ' ')
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu alana sadece harf girişi yapılabilir.");
+                        txtUrunMarkasi.Text = txtUrunMarkasi.Text.Substring(0, txtUrunMarkasi.Text.Length - 1);
+                        txtUrunMarkasi.Select(txtUrunMarkasi.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void txtUrunAdi_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            foreach (char harf in txtUrunAdi.Text)
+            {
+                if (!char.IsLetter(harf))
+                {
+                    if (harf == ' ')
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu alana sadece harf girişi yapılabilir.");
+                        txtUrunAdi.Text = txtUrunAdi.Text.Substring(0, txtUrunAdi.Text.Length - 1);
+                        txtUrunAdi.Select(txtUrunAdi.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void txtAlisFiyati_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            foreach (char sayi in txtAlisFiyati.Text)
+            {
+                if (!char.IsDigit(sayi))
+                {
+                    if (sayi == ',')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu alana sadece rakam girişi yapılabilir.");
+                        txtAlisFiyati.Text = txtAlisFiyati.Text.Substring(0, txtAlisFiyati.Text.Length - 1);
+                        txtAlisFiyati.Select(txtAlisFiyati.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+            int Sayac = 0;
+            for (int i = 0; i < txtAlisFiyati.Text.Length; i++)
+            {
+                if (txtAlisFiyati.Text[i] == ',')
+                {
+                    Sayac++;
+                    if (Sayac > 1)
+                    {
+                        MessageBox.Show("Bu alana sadece bir adet virgül girilebilir.");
+                        txtAlisFiyati.Text = txtAlisFiyati.Text.Substring(0, txtAlisFiyati.Text.Length - 1);
+                        txtAlisFiyati.Select(txtAlisFiyati.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void txtSatisFiyati_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            foreach (char sayi in txtSatisFiyati.Text)
+            {
+                if (!char.IsDigit(sayi))
+                {
+                    if (sayi == ',')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bu alana sadece rakam girişi yapılabilir.");
+                        txtSatisFiyati.Text = txtSatisFiyati.Text.Substring(0, txtSatisFiyati.Text.Length - 1);
+                        txtSatisFiyati.Select(txtSatisFiyati.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+            int Sayac = 0;
+            for (int i = 0; i < txtAlisFiyati.Text.Length; i++)
+            {
+                if (txtSatisFiyati.Text[i] == ',')
+                {
+                    Sayac++;
+                    if (Sayac > 1)
+                    {
+                        MessageBox.Show("Bu alana sadece bir adet virgül girilebilir.");
+                        txtSatisFiyati.Text = txtSatisFiyati.Text.Substring(0, txtSatisFiyati.Text.Length - 1);
+                        txtSatisFiyati.Select(txtSatisFiyati.Text.Length, 0);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
