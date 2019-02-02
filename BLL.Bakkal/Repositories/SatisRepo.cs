102a392c5e6ba7f0eb913ef0dab555714b1ec370
mertcelil <M720694@outlook.com>
2019-02-02 20:53:59 +0300

    satış ekranının bll katmanı için satisrepo oluşturuldu

---
+++ BLL.Bakkal/Repositories/SatisRepo.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Bakkal.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Bakkal.Repositories
+{
+    public class SatisRepo
+    {
+        BCDContext ent = new BCDContext();
+        public Urunler UrunBul(string UrunKod)
+        {
+            return ent.Urunlers.Where(u => u.UrunKodu == UrunKod).FirstOrDefault();
+        }
+    }
+}
